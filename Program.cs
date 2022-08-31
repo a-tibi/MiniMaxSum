bbfae319e4ffa0c376833cee63457b65ce175775
a-tibi <ahmd_tibi@yahoo.com>
2022-08-31 10:57:58 +0400

    commit all files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniMaxPlus
+{
+	class Result
+	{
+		public static void miniMaxSum(List<int> arr)
+		{
+			var sum = new List<int>();
+
+			foreach (var item in arr)
+			{
+				sum.Add(arr.Sum() - item);
+			}
+
+			Console.WriteLine($"Minimum Sum: {sum.Min()} - Maximum Sum: {sum.Max()}");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+			Result.miniMaxSum(arr);
+
+			Console.ReadKey();
+		}
+	}
+}
